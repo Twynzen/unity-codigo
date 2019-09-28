e37d60f95c1260e0e3c71554fba72f51d2f2b220
Twynzen <razzziel001@gmail.com>
2019-09-28 13:52:18 -0500

    Primeros pasos para darle movimiento a una  barra 3d con unity

---
+++ Barra.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barra : MonoBehaviour
+{   
+    public float velocidad = 0.4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+  
+    void Update()
+    {
+        //el getaxisRaw recibimos un valor -1 si se esta pulsando 0 si no se pulza nada y 1 si se esta pulsando el eje positivo
+        float tecladoHorizontal = Input.GetAxisRaw("Horizontal");
+        float posX = transform.position.x + (tecladoHorizontal * velocidad* Time.deltaTime);
+        Debug.Log("Uptade");
+    }
+}
