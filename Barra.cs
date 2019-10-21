7550ec00612bbfb00a948dacc980650c320170d8
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2019-10-20 22:49:59 -0500

    Add files via upload
    Volviendo con la barra y su función de moverse

--- Barra.cs
+++ Barra.cs
@@ -3,21 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Barra : MonoBehaviour
-{   
+{
+
     public float velocidad = 0.4f;
+
+    public Vector3 posicionInicial;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Start");
+       posicionInicial = transform.position;
     }
-
-  
+    public void Reset()
+    {
+        transform.position = posicionInicial;
+    }
+    // Update is called once per frame
     void Update()
     {
-        //el getaxisRaw recibimos un valor -1 si se esta pulsando 0 si no se pulza nada y 1 si se esta pulsando el eje positivo
         float tecladoHorizontal = Input.GetAxisRaw("Horizontal");
-        float posX = transform.position.x + (tecladoHorizontal * velocidad* Time.deltaTime);
-        Debug.Log("Uptade");
+        float posX = transform.position.x + (tecladoHorizontal * velocidad *Time.deltaTime);
+        transform.position = new Vector3(Mathf.Clamp(posX, -8, 8), transform.position.y, transform.position.z);
+
     }
 }
