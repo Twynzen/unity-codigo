f77846ac617a604e76a86d9c02a7bdc38c3ddc8d
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2019-10-21 14:16:30 -0500

    Add files via upload
    La pelota esta en proceso de tener físicas, cuando se presiona ctrl izq  deja de ser la pelota hija del objeto barra.

---
+++ Pelota.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pelota : MonoBehaviour
+{
+    public float velocidadInicial = 600f;
+    public Rigidbody rig;
+
+    bool enJuego = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!enJuego && Input.GetButtonDown("Fire1")) 
+        {
+            enJuego = true;
+            transform.SetParent(null);
+            rig.isKinematic = false;
+            rig.AddForce(new Vector3(velocidadInicial, velocidadInicial, 0)); 
+        }
+    }
+}
