d7ab9a707a3c66db84ff2080a4034da8ccb8aa22
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2019-11-10 19:56:52 -0500

    Add files via upload

---
+++ Bloque.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloque : MonoBehaviour
+{
+
+    public GameObject efectoParticulas;
+    //el triger desactivado
+    void OnCollisionEnter(){
+
+        Instantiate(efectoParticulas, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+
+    }
+
+    /*el triger esta activado
+    void OnTriggerEnter(){
+
+        Debug.Log("TriggerEnter");
+
+    }
+*/
+}
