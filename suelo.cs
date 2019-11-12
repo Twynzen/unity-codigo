a9650c0edd6e3aac502d61c1b630a85148592e96
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2019-11-12 18:23:14 -0500

    El código para perder vidas y reiniciar la pelota al perder

---
+++ suelo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class suelo : MonoBehaviour
+{
+    public vidas Vidas;
+
+   void OnTriggerEnter(){
+       
+        Vidas.PerderVida();
+
+   }
+    
+}
---
+++ vidas.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class vidas : MonoBehaviour
+{
+
+    public static int Vidas = 3;
+
+    public Text textoVidas;
+
+    public Pelota pelota;
+
+    public Barra barra;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ActualizarMarcadorVidas();
+    }
+
+    void ActualizarMarcadorVidas(){
+
+        textoVidas.text = "Vidas: "+ vidas.Vidas;
+    }
+ 
+    public void PerderVida(){
+
+        vidas.Vidas--;
+        ActualizarMarcadorVidas();
+        barra.Reset();
+        pelota.Reset();
+
+    }
+}
