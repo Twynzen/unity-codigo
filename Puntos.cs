c851da7ca2605cc0ba07caafe4b220ed1309b11f
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2019-11-17 19:48:54 -0500

    configure código de  puntos
    Al golpear un bloque aparecen los puntos en la interfaz

---
+++ Puntos.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Puntos : MonoBehaviour
+{
+    public static int puntos = 0;
+    public Text textoPuntos;
+
+    void Start()
+    {
+        ActualizarMarcadorPuntos(); 
+    }
+
+    void ActualizarMarcadorPuntos(){
+        textoPuntos.text="Puntos: " + Puntos.puntos;
+    }
+
+    public void GanarPunto(){
+        Puntos.puntos++;
+        ActualizarMarcadorPuntos();
+    }
+}
