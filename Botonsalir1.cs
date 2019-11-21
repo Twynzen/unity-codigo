5f27366b33bdc2df2cc595e62e33785737cc7bda
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2019-11-20 22:54:13 -0500

    Configurando portada

--- Botonsalir1.cs
+++ Botonsalir1.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Botonsalir1 : MonoBehaviour
 {
-    
+    public bool salir;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            if (salir){
             Debug.Log("Salimos del juego");
             Application.Quit();
+            }else{
+                
+                Application.LoadLevel("Portada");
+
+            }
         }
     }
 }
---
+++ EmpezarPartida.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmpezarPartida : MonoBehaviour
+{
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Puntos.puntos = 0;
+            vidas.Vidas = 3;
+            Application.LoadLevel("Nivel1");
+        }
+    }
+}
