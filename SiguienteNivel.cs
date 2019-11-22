045a10b426e0f6ea9ce45e3cd2a1a6679389cbf4
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2019-11-21 19:08:48 -0500

    Un intervalo de tiempo al cambiar un nivel

---
+++ SiguienteNivel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SiguienteNivel : MonoBehaviour
+{
+
+    public string nivelACargar;
+    public float retraso;
+    [ContextMenu("Activar Carga")]
+    public void ActivarCarga(){
+        Invoke("CargarNivel", retraso);
+    }
+
+    void CargarNivel(){
+        Application.LoadLevel(nivelACargar);
+    }
+
+    public bool EsUltimoNivel(){
+        if (nivelACargar == "Portada")
+        {
+            return true;
+        }else
+        {
+            return false;
+        }
+        
+    }
+}
