54685d0c5dae4221e48dea24be206688aa081366
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2019-11-29 18:39:55 -0500

    Sonidos de perder y de ganar, falta arreglar los metodos

---
+++ sonidosFinPartida.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sonidosFinPartida : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip completado;
+    public AudioClip gameOver;
+   //aquí llamaremos los metodos para hacer sonar cada clip
+}
