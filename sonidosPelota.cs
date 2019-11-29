a02e349b7d6e71d44609264d80df8f0f998edac8
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2019-11-28 20:52:07 -0500

    Colocando sonidos de rebote y puntos

---
+++ sonidosPelota.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sonidosPelota : MonoBehaviour{
+
+    public AudioSource rebote;
+    public AudioSource punto;
+
+    void OnCollisionEnter(Collision otro){
+
+        if (otro.gameObject.CompareTag("Bloque"))
+        {   
+            Debug.Log("pum");
+            punto.Play();
+        }
+        else
+        {
+            rebote.Play(); 
+        }
+
+    }
+}
